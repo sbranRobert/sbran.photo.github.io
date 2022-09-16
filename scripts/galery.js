9f76e7104b33c6ab27ec48e96ddd4de2b39009e9
stephanebran <stephane.bran@talan.com>
2022-09-16 10:22:09 +0200

    fix gallery

--- scripts/galery.js
+++ scripts/galery.js
@@ -1,60 +1,13 @@
-const imgList = [
-    {
-        filename: 'COUCOU',
-        path: 'images/COUCOU.jpg',
-        alt: 'Ecureuil',
-        type: 'animals'
-    },
-    {
-        filename: 'l_ecureuil_cache',
-        path: 'images/l_ecureuil_cache.jpg',
-        alt: 'L\'écurueil caché',
-        type: 'paysage'
-    },
-    {
-        filename: 'le_vole_bleu',
-        path: 'images/le_vole_bleu.jpg',
-        alt: 'Le vole bleu',
-        type: 'animals'
-    },
-    {
-        filename: 'spiders',
-        path: 'images/spiders.jpg',
-        alt: 'Les deux araignées',
-        type: 'paysage'
-    },
-    {
-        filename: 'l_emmerdeur',
-        path: 'images/l_emmerdeur.jpg',
-        alt: 'L\'emmerdeur',
-        type: 'paysage'
-    },
-    {
-        filename: 'la_mouette_tendue',
-        path: 'images/la_mouette_tendue.jpg',
-        alt: 'La mouette tendue',
-        type: 'animals'
-    },
-    {
-        filename: 'le_prof',
-        path: 'images/le_prof.jpg',
-        alt: 'Le prof',
-        type: 'animals'
-    },
-    {
-        filename: 'le_regard',
-        path: 'https://live.staticflickr.com/65535/51922807445_91ec53ee16_z.jpg',
-        alt: 'Le regard',
-        type: 'paysage'
-    },
-]
-
-const popup = document.getElementById('popup');
+import * as photoLists from './models.js';
 
 let photoType = 'animals';
 
+const popup = document.getElementById('popup');
+
+popup.addEventListener('click', () => closePopup());
+
 function closePopup() {
-    popup.textContent = "";
+    popup.textContent = '';
     popup.style.visibility = 'hidden';
     popup.style.opacity = '0';
     popup.style.animation = 'fadeOut 200ms';
@@ -71,22 +24,30 @@ document.getElementById('type-select').addEventListener('change', function() {
 });
 
 function reloadGallery() {
-    imgList.filter(img => img.type === photoType).forEach(function (file) {
+    let imgList;
+    switch (photoType) {
+        case "animals":
+            imgList = photoLists.animalsImgList;
+            break;
+        default:
+            imgList = photoLists.paysageImgList;
+    }
+    imgList.forEach(function (file) {
 
-        const pictureDiv = document.createElement("div");
-        pictureDiv.setAttribute("class", "picture");
+        const pictureDiv = document.createElement('div');
+        pictureDiv.setAttribute('class', 'picture');
 
-        const img = document.createElement("img");
-        img.setAttribute("src", file.path);
-        img.setAttribute("alt", file.alt);
-        img.setAttribute("id", file.filename);
-        img.setAttribute("loading", "lazy");
+        const img = document.createElement('img');
+        img.setAttribute('src', file.path);
+        img.setAttribute('alt', file.alt);
+        img.setAttribute('id', file.filename);
+        img.setAttribute('loading', 'lazy');
         img.addEventListener('click', () => {
             const imgPopup = document.getElementById(file.filename).cloneNode(true);
 
-            imgPopup.style.maxHeight = "90vh";
-            imgPopup.style.maxWidth = "90vw";
-            imgPopup.style.marginTop = "50vh";
+            imgPopup.style.maxHeight = '90vh';
+            imgPopup.style.maxWidth = '90vw';
+            imgPopup.style.marginTop = '50vh';
             imgPopup.style.transform = 'translateY(-50%)';
 
             popup.style.visibility = 'visible';
---
+++ scripts/models.js
@@ -0,0 +1,55 @@
+export const animalsImgList = [
+    {
+        filename: 'COUCOU',
+        path: 'images/COUCOU.jpg',
+        alt: 'Ecureuil'
+    },
+    {
+        filename: 'le_vole_bleu',
+        path: 'images/le_vole_bleu.jpg',
+        alt: 'Le vole bleu'
+    },
+    {
+        filename: 'la_mouette_tendue',
+        path: 'images/la_mouette_tendue.jpg',
+        alt: 'La mouette tendue'
+    },
+    {
+        filename: 'le_prof',
+        path: 'images/le_prof.jpg',
+        alt: 'Le prof'
+    },
+];
+
+export const paysageImgList = [
+    {
+        filename: 'l_emmerdeur',
+        path: 'images/l_emmerdeur.jpg',
+        alt: 'L\'emmerdeur',
+    },
+    {
+        filename: 'l_arbre_en_foret',
+        path: 'https://live.staticflickr.com/65535/51865531476_a1e86fb9a8_h.jpg',
+        alt: 'L\'arbre de la forêt',
+    },
+    {
+        filename: 'le_regard',
+        path: 'https://live.staticflickr.com/65535/51922807445_91ec53ee16_z.jpg',
+        alt: 'Le regard',
+    },
+    {
+        filename: 'couche_soleil',
+        path: 'https://live.staticflickr.com/65535/51910464578_51906f84a3_h.jpg',
+        alt: 'Le couché de soleil sur les toits de Paris',
+    },
+    {
+        filename: 'l_arbre_crochu',
+        path: 'https://live.staticflickr.com/65535/51865847509_0007cb3e52_k.jpg',
+        alt: 'L\'arbre crochu',
+    },
+    {
+        filename: 'eiffel-tour_by_night',
+        path: 'https://live.staticflickr.com/65535/50398034657_524d6aa4c7_k.jpg',
+        alt: 'La tour Eiffel de nuit',
+    },
+]
