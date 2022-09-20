82a298567ef35c6a9ed421aa77108714b5b89272
treborkaa <stephane.bran.photo@gmail.com>
2022-09-20 09:31:56 +0200

    rework css and galery menu

--- scripts/galery.js
+++ scripts/galery.js
@@ -1,33 +1,38 @@
 import * as photoLists from './models.js';
 
-let photoType = 'animals';
+let photoType = 'paysage';
+
+const menuElements = document.querySelectorAll('.type-element');
+
+menuElements.forEach(element => {
+    element.addEventListener('click', (event) => {
+        document.getElementById('type-menu-active').removeAttribute('id');
+        element.setAttribute('id', 'type-menu-active');
+        photoType = event.target.getAttribute('value');
+        pictureList.innerHTML = null;
+        reloadGallery();
+    });
+});
 
 const popup = document.getElementById('popup');
 
 popup.addEventListener('click', () => closePopup());
 
-function closePopup() {
-    popup.textContent = '';
-    popup.style.visibility = 'hidden';
-    popup.style.opacity = '0';
-    popup.style.animation = 'fadeOut 200ms';
-}
-
 const pictureList = document.getElementById('picture-list');
 
-reloadGallery();
+const loaderDiv = document.createElement('div');
+loaderDiv.setAttribute('id', 'load');
+// loaderDiv.innerHTML = '<img data-src="https://ik.imagekit.io/demo/default-image.jpg">';
+loaderDiv.style.zIndex = '10';
 
-document.getElementById('type-select').addEventListener('change', function() {
-    photoType = this.value;
-    pictureList.innerHTML = null;
-    reloadGallery();
-});
-
-function reloadGallery() {
+const reloadGallery = () => {
     let imgList;
     switch (photoType) {
-        case "animals":
+        case 'animals':
             imgList = photoLists.animalsImgList;
+            break;
+        case 'architecture':
+            imgList = photoLists.architectureImgList;
             break;
         default:
             imgList = photoLists.paysageImgList;
@@ -41,7 +46,9 @@ function reloadGallery() {
         img.setAttribute('src', file.path);
         img.setAttribute('alt', file.alt);
         img.setAttribute('id', file.filename);
+        img.setAttribute('class', 'galery-img');
         img.setAttribute('loading', 'lazy');
+        img.style.visibility = 'hidden';
         img.addEventListener('click', () => {
             const imgPopup = document.getElementById(file.filename).cloneNode(true);
 
@@ -57,6 +64,35 @@ function reloadGallery() {
         });
 
         pictureDiv.appendChild(img);
+        pictureDiv.appendChild(loaderDiv.cloneNode(true));
         pictureList.appendChild(pictureDiv);
+        
     });
+
+    const imgInGalery = document.querySelectorAll('.galery-img');
+
+    document
+        .querySelectorAll('.galery-img')
+        .forEach(img => 
+            img.addEventListener('load', (e) => {
+                img.style.visibility = 'visible';
+                const imgParent = img.parentNode;
+                imgParent.querySelector('#load').remove();
+            })
+        );
+}
+
+const closePopup = () => {
+    popup.textContent = '';
+    popup.style.visibility = 'hidden';
+    popup.style.opacity = '0';
+    popup.style.animation = 'fadeOut 200ms';
+}
+
+reloadGallery();
+
+async function lightBox(url) {
+  lightboxElem.style.display = "block";
+  await loadImage("https://somesite.net/huge-image.jpg", lightboxImgElem);
+  lightboxControlsElem.disabled = false;
 }
